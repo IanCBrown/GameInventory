bd19974f7d0ebd07f071192ae4d28c4c8e6834c7
Ian Brown <icb0004@auburn.edu>
2020-08-13 21:29:39 -0500

    Changed namespace

--- GameInventory/GameItem/ItemsEnum.cs
+++ GameInventory/GameItem/ItemsEnum.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace GameInventory.Item
+namespace GameInventory.GameItem
 {
     public enum Items
     {
--- GameInventory/Program.cs
+++ GameInventory/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using GameInventory.GameItem;
 
 namespace GameInventory
 {
